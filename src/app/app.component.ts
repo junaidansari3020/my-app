016d8af55dccde5f4b4de0f6ead36a61d140b8af
junaid ansari <junaid@gmail.com>
2025-02-14 00:23:18 +0530

    version 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CommonModule } from '@angular/common';
-
+import { HttpResponse } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -17,9 +18,45 @@ import { CommonModule } from '@angular/common';
     DashboardComponent,
     CommonModule,
     RouterOutlet,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public firstName: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    const accessToken = {
+      iRequestID: 1023,
+    };
+
+    this.authService.loginUser(accessToken).subscribe({
+      next: (response: HttpResponse<any>) => {
+        console.log('Access Token:', response);
+        this.dashboard();
+      },
+      error: (error) => {
+        console.error('Access Token failed:', error);
+      },
+    });
+  }
+
+  dashboard(){
+    const getUserData = {
+      iRequestID: 2036,
+    };
+
+    this.authService.dataUser(getUserData).subscribe({
+      next: (response: HttpResponse<any>) => {
+        console.log('user data:', response.body[0].sFirstName);
+        this.firstName = response.body[0].sFirstName;
+      },
+      error: (error) => {
+        console.error('user data failed:', error);
+      },
+    });
+  }
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private loginUrl: string = 'https://car-service.in/luxuryfix/boauth';
+  private boauthUrl: string = 'https://car-service.in/luxuryfix/boauth';
+  private bodataUrl: string = 'https://car-service.in/luxuryfix/bodata';
 
   constructor(private router: Router, private http: HttpClient) {}
 
@@ -18,6 +19,16 @@ export class AuthService {
       observe: 'response' as const 
     };
 
-    return this.http.post<any>(this.loginUrl, data, options);
+    return this.http.post<any>(this.boauthUrl, data, options);
+  }
+
+  public dataUser(data: any, headers?: HttpHeaders): Observable<HttpResponse<any>> {
+    const options = {
+      headers: headers || new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' }),
+      withCredentials: true,
+      observe: 'response' as const 
+    };
+
+    return this.http.post<any>(this.bodataUrl, data, options);
   }
 }
